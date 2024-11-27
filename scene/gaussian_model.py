209018fed753455c7753c169402e8f9b1196ca5b
onetwohour <chan8645@naver.com>
2024-11-27 14:42:41 +0900

    test value

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -152,17 +152,17 @@ class GaussianModel:
         self.denom = torch.zeros((self.get_xyz.shape[0], 1), device="cuda")
 
         l = [
-            {'params': [self._xyz], 'lr': training_args.position_lr_init * self.spatial_lr_scale * pow(1e-4, self.style_train), "name": "xyz"},
+            {'params': [self._xyz], 'lr': training_args.position_lr_init * self.spatial_lr_scale * pow(1e-3, self.style_train), "name": "xyz"},
             {'params': [self._features_dc], 'lr': training_args.feature_lr, "name": "f_dc"},
-            {'params': [self._features_rest], 'lr': training_args.feature_lr / 20.0 * pow(1e-4, self.style_train), "name": "f_rest"},
-            {'params': [self._opacity], 'lr': training_args.opacity_lr * pow(1e-4, self.style_train), "name": "opacity"},
-            {'params': [self._scaling], 'lr': training_args.scaling_lr * pow(1e-4, self.style_train), "name": "scaling"},
-            {'params': [self._rotation], 'lr': training_args.rotation_lr * pow(1e-4, self.style_train), "name": "rotation"}
+            {'params': [self._features_rest], 'lr': training_args.feature_lr / 20.0 * pow(1e-3, self.style_train), "name": "f_rest"},
+            {'params': [self._opacity], 'lr': training_args.opacity_lr * pow(1e-3, self.style_train), "name": "opacity"},
+            {'params': [self._scaling], 'lr': training_args.scaling_lr * pow(1e-3, self.style_train), "name": "scaling"},
+            {'params': [self._rotation], 'lr': training_args.rotation_lr * pow(1e-3, self.style_train), "name": "rotation"}
         ]
 
         self.optimizer = torch.optim.Adam(l, lr=0.0, eps=1e-15)
-        self.xyz_scheduler_args = get_expon_lr_func(lr_init=training_args.position_lr_init*self.spatial_lr_scale,
-                                                    lr_final=training_args.position_lr_final*self.spatial_lr_scale,
+        self.xyz_scheduler_args = get_expon_lr_func(lr_init=training_args.position_lr_init*self.spatial_lr_scale * pow(1e-3, self.style_train),
+                                                    lr_final=training_args.position_lr_final*self.spatial_lr_scale * pow(1e-3, self.style_train),
                                                     lr_delay_mult=training_args.position_lr_delay_mult,
                                                     max_steps=training_args.position_lr_max_steps)
 
